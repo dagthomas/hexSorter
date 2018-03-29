0d63ee5b1b8364dfd1fa46962a58299622ffb19d
dagthomas <dagthomas@gmail.com>
2018-03-29 15:30:27 +0200

    fixing stuff, adding images

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # hexSorter
 
-```
+```javascript
 var colorArray = ["#516373", "#6c838c", "#f2e8c9", "#f2b999", "#f2f2f2"];
 
-console.log("bright", mostBrightColor(colorArray));
-console.log("saturated", mostSaturatedColor(colorArray));
-console.log("bright dull", brightestDullColor(colorArray));
-console.log("mix", mix_colors('#000000', '#FF0000', 65));
+console.log("bright", module.exports.mostBrightColor(colorArray));
+console.log("saturated", module.exports.mostSaturatedColor(colorArray));
+console.log("bright dull", module.exports.brightestDullColor(colorArray));
+console.log("mix", module.exports.colorMixer('#000000', '#FF0000', 65));
 
 ```
 
--- convtocss.js
+++ convtocss.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const readline = require('readline');
-const hexfix = require('./hexfix');
+const hexSorter = require('./hexSorter');
 
 const outputFile = 'dagthomas.css';
 const inputFile = 'colors.txt';
 
 var lineno = 1;
-var output;
+var output = "";
+
+fs.unlink(outputFile, function(err) {
+    if (err && err.code == 'ENOENT') {
+        //  console.info("File " + outputFile + " doesn't exist, won't remove it.");
+    } else if (err) {
+        //   console.error("Error occurred while trying to remove " + outputFile);
+    } else {
+        //   console.info(`removed ` + outputFile);
+    }
+});
 
 var lineReader = readline.createInterface({
     input: fs.createReadStream(inputFile)
@@ -20,31 +30,43 @@ function writeToFile(input) {
 }
 
 lineReader.on('line', function(line) {
-    output = "";
     var hl1, hl2, fill, s1, s2;
     a = line.split(",");
-    h1 = hexfix.mostBrightColor(a);
+    h1 = hexSorter.mostBrightColor(a);
     a.splice(a.indexOf(h1), 1);
-    h2 = hexfix.mostBrightColor(a);
+    h2 = hexSorter.mostBrightColor(a);
     a.splice(a.indexOf(h2), 1);
-    fill = hexfix.mostBrightColor(a);
+    fill = hexSorter.mostBrightColor(a);
     a.splice(a.indexOf(fill), 1);
-    s1 = hexfix.mostBrightColor(a);
+    s1 = hexSorter.mostBrightColor(a);
     a.splice(a.indexOf(s1), 1);
-    s2 = hexfix.mostBrightColor(a);
+    s2 = hexSorter.mostBrightColor(a);
     a.splice(a.indexOf(s2), 1);
 
-    output += ".palette" + lineno + ".h1c {" + "color: " + h1 + ";}\r\n";
-    output += ".palette" + lineno + ".h1bg {" + "background: " + h1 + ";}\r\n";
-    output += ".palette" + lineno + ".h2c {" + "color: " + h2 + ";}\r\n";
-    output += ".palette" + lineno + ".h2bg {" + "background: " + h2 + ";}\r\n";
-    output += ".palette" + lineno + ".fillc {" + "color: " + fill + ";}\r\n";
-    output += ".palette" + lineno + ".fillbg {" + "background: " + fill + ";}\r\n";
-    output += ".palette" + lineno + ".s1c {" + "color: " + s1 + ";}\r\n";
-    output += ".palette" + lineno + ".s1bg {" + "background: " + s1 + ";}\r\n";
-    output += ".palette" + lineno + ".s2c {" + "color: " + s2 + ";}\r\n";
-    output += ".palette" + lineno + ".s2bg {" + "background: " + s2 + ";}\r\n";
+    output += "/* Palette #" + lineno + " */\r\n";
+    output += "._dtp" + lineno + ".h1c {" + "color:       " + h1 + ";}\r\n";
+    output += "._dtp" + lineno + ".h1bg {" + "background: " + h1 + ";}\r\n";
+    output += "._dtp" + lineno + ".h1f {" + "fill:        " + h1 + ";}\r\n";
+    output += "._dtp" + lineno + ".h1s {" + "stroke:      " + h1 + ";}\r\n";
+    output += "._dtp" + lineno + ".h2c {" + "color:       " + h2 + ";}\r\n";
+    output += "._dtp" + lineno + ".h2bg {" + "background: " + h2 + ";}\r\n";
+    output += "._dtp" + lineno + ".h2f {" + "fill:        " + h2 + ";}\r\n";
+    output += "._dtp" + lineno + ".h2s {" + "stroke:      " + h2 + ";}\r\n";
+    output += "._dtp" + lineno + ".fc {" + "color:        " + fill + ";}\r\n";
+    output += "._dtp" + lineno + ".fbg {" + "background:  " + fill + ";}\r\n";
+    output += "._dtp" + lineno + ".ff {" + "fill:         " + fill + ";}\r\n";
+    output += "._dtp" + lineno + ".fs {" + "stroke:       " + fill + ";}\r\n";
+    output += "._dtp" + lineno + ".s1c {" + "color:       " + s1 + ";}\r\n";
+    output += "._dtp" + lineno + ".s1bg {" + "background: " + s1 + ";}\r\n";
+    output += "._dtp" + lineno + ".s1f {" + "fill:        " + s1 + ";}\r\n";
+    output += "._dtp" + lineno + ".s1s {" + "stroke:      " + s1 + ";}\r\n";
+    output += "._dtp" + lineno + ".s2c {" + "color:       " + s2 + ";}\r\n";
+    output += "._dtp" + lineno + ".s2bg {" + "background: " + s2 + ";}\r\n";
+    output += "._dtp" + lineno + ".s2f {" + "fill:        " + s2 + ";}\r\n";
+    output += "._dtp" + lineno + ".s2s {" + "stroke:      " + s2 + ";}\r\n\r\n";
 
-    writeToFile(output);
     lineno++;
+});
+lineReader.on('close', function() {
+    writeToFile(output);
 });
